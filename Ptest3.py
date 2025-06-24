7962a3960a3c57d880fd103ce6ae20a07e0c1d00
Kmh <munhin.koo@qiu.edu.my>
2025-06-24 23:03:28 +0800

    Third commit

---
+++ Ptest3.py
@@ -0,0 +1,7 @@
+# a py code initiaze two numbers, and compare which is greater
+num1 = 10
+num2 = 15
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num2} is greater than {num1}")
