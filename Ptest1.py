6637d101571c9772ab973d2709abad19a0561bb3
Kmh <munhin.koo@qiu.edu.my>
2025-06-24 22:41:53 +0800

    First commit

---
+++ Ptest1.py
@@ -0,0 +1,10 @@
+print("Hello world")
+# This is a simple Python script to add two numbers.
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+def add(a, b):
+    """Function to add two numbers."""
+    return a + b
+
+result = add(num1, num2)
+print("The sum is:", result)
